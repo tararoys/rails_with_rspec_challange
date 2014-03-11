9ffa2decc69a01e59ab52f204ca2b74a428bdb77
Tara Roys <tlroys@gmail.com>
2014-03-11 18:16:20 -0500

    Refactorinng the Post tests to be DRYer.

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,21 +1,11 @@
 require 'spec_helper'
 
 describe Post do
-
-  #lazy loaded variable. This only gets created if a test asks for it. 
+ 
   let(:valid_params) {{title: "New post!", content: "A great story"}}
 
-  #creates an instance variable for each and every test in this suite.
-  # before do
-    # @valid_params = {title: "New post!", content: "A great story"}
-  # end
 
   it "title should be automatically titleized before save" do
-    valid_params[:title] = 'new post'
-    #create a custom hash to pass in just what this test needs
-    #post = Post.new(@valid_params)
-
-    #merge operation overwrites specifiec things in a hash! 
     post = Post.new(valid_params.merge(title: 'new post'))
     post.save
     expect(post.title).to eq("New Post")
